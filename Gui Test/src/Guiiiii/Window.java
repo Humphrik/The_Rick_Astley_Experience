106bf666ead9cb35e442cd3d233f5221b2f6e70e
Kenneth Humphries <kennyboyeleete@gmail.com>
2016-01-29 14:53:57 -0500

    Comments and stuff

--- Gui Test/src/Guiiiii/Window.java
+++ Gui Test/src/Guiiiii/Window.java
@@ -33,47 +33,47 @@ public class Window {
 	static Boolean reopen = false;
 	static Clip clip, clip2;
 	// static WindowListener exitListener;
+	//static MalformedURLException error = new MalformedURLException("");
 
-	static MalformedURLException error = new MalformedURLException("");
-
-	public static void main(String[] args) throws MalformedURLException {
-		hauntedURL = new URL("https://media.giphy.com/media/1dPTVv6FaQmZ2/giphy.gif");
+	public static void main(String[] args) throws MalformedURLException { //Accommodates URL errors.
+		hauntedURL = new URL("https://media.giphy.com/media/1dPTVv6FaQmZ2/giphy.gif"); //???
 		URL url = new URL(
-				"http://2.bp.blogspot.com/-baqmxAt8YHg/UMRuNx6uNdI/AAAAAAAAD1s/TzmvfnYyP8E/s1600/rick-astely.gif");
+				"http://2.bp.blogspot.com/-baqmxAt8YHg/UMRuNx6uNdI/AAAAAAAAD1s/TzmvfnYyP8E/s1600/rick-astely.gif"); //Ricky
 		icon = new ImageIcon(url);
 		label2 = new JLabel(icon);
-		play("src\\Guiiiii\\Ricky.wav", true);
-		playOther("src\\Guiiiii\\xfiles.wav", false);
-		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		//Image created.
+		play("src\\Guiiiii\\Ricky.wav", true); //Creates and plays main music.
+		playOther("src\\Guiiiii\\xfiles.wav", false); //???
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); //Makes sure the window does not terminate early.
 		frame.setSize(1280, 800);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		button.addActionListener(new Listener());
+		button.addActionListener(new Listener()); //Adds function to button.
 		panel.add(label);
 		panel.add(button);
 		panel.add(label2);
-		frame.add(panel);
+		frame.add(panel); //Everything put together.
 		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent evt) {
+			public void windowClosing(WindowEvent evt) { //Upon attempting to close early.
 				clip.stop();
 				// clip2.start();
 
-				reopen = true;
+				reopen = true; //This is to avoid issues and allow time to updated.
 			}
 		});
-		frame.setVisible(true);
-		while (spooky) {
+		frame.setVisible(true); //Displays window.
+		while (spooky) { //While running functionally.
 			System.out.print("");
 			if (reopen) {
-				reopen();
+				reopen(); //Reopens a 'closed' window.
 				reopen = false;
 			}
 		}
-		spooked();
+		spooked(); //???
 
 	}
 
-	public static void play(String filename, Boolean startup) {
+	public static void play(String filename, Boolean startup) { //Creates music for clip.
 		try {
 			clip = AudioSystem.getClip();
 			clip.open(AudioSystem.getAudioInputStream(new File(filename)));
@@ -86,13 +86,13 @@ public class Window {
 		}
 	}
 
-	public static void playOther(String filename, Boolean startup) {
+	public static void playOther(String filename, Boolean startup) { //Creates music for clip 2.
 		try {
 			clip2 = AudioSystem.getClip();
 			clip2.open(AudioSystem.getAudioInputStream(new File(filename)));
 			clip2.start();
 			if (!startup) {
-				clip2.stop();
+				clip2.stop(); //Stops.
 			}
 		} catch (Exception exc) {
 			exc.printStackTrace(System.out);
@@ -101,7 +101,7 @@ public class Window {
 
 	// public static void close() {
 	// }
-	public static void spooked() {
+	public static void spooked() { //For the unlucky.
 		try {
 			clip.stop();
 			clip2.start();
@@ -109,14 +109,14 @@ public class Window {
 		} catch (InterruptedException ex) {
 		}
 		frame.setVisible(false);
-		while (clip2.isRunning()) {
+		while (clip2.isRunning()) { //???
 			System.out.print("");
 		}
 		System.out.println("I want to believe.");
 		frame.dispose();
 	}
 
-	public static void reopen() {
+	public static void reopen() { // "Reopens" after a delay.
 		try {
 			Thread.sleep(6000);
 			clip.start();
@@ -130,20 +130,18 @@ public class Window {
 	}
 }
 
-class Listener implements ActionListener {
+class Listener implements ActionListener { //For when the button is pressed. 
 	private double click;
 	private int clickCount = 0;
-	private static Icon hauntedIcon = new ImageIcon(Window.hauntedURL);
+	private static Icon hauntedIcon = new ImageIcon(Window.hauntedURL); //ooooOOOOhhh.
 
 	public void actionPerformed(ActionEvent e) {
 		click = Math.random();
-		clickCount++;
-		if (Window.label2.getIcon() == hauntedIcon) {
-		}
+		clickCount++; //Adds to click count.
 		if (clickCount == 50) {
-			Window.label.setText("You suck.");
+			Window.label.setText("You suck."); //Easter egg.
 		} else if (clickCount == 75) {
-			Window.label.setText("Give up.");
+			Window.label.setText("Give up."); //Easter egg.
 		} else if (clickCount >= 100 && clickCount <= 108) {
 			Window.label.setText("N3VER GONN4 G1VE Y0U UP!1!!11!");
 		} else if (clickCount > 108 && clickCount <= 116) {
@@ -152,17 +150,17 @@ class Listener implements ActionListener {
 			Window.label.setText("N___R GOn54 RN aR##Nd");
 		} else if (clickCount > 124 && clickCount <= 132) {
 			Window.label.setText("AND D3S3RT YYYYYYY UUI YP UI YIUY JKSDH");
-		} else if (clickCount > 132) {
+		} else if (clickCount > 132) { //You've gone too far...
 			Window.label2.setIcon(hauntedIcon);
 			Window.spooky = false;
 		} else {
-			Window.label.setText("" + clickCount);
+			Window.label.setText("" + clickCount); //Displays the number of clicks (usually.)
 		}
 		System.out.println(click);
-		if (click >= 0.99) {
+		if (click >= 0.99) { //When you are lucky enough....
 			Window.label.setText("You may Leave.");
-			Window.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			Window.button.setEnabled(false);
+			Window.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Window will no longer "reopen."
+			Window.button.setEnabled(false); //Turns off button.
 
 		}
 
