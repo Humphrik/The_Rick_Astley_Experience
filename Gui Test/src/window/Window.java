4592077564b81a139065fbe14a5a20d83492e914
Kenneth Humphries <kennyboyeleete@gmail.com>
2016-01-30 10:23:29 -0500

    Removed unneeded imports.

--- Gui Test/src/window/Window.java
+++ Gui Test/src/window/Window.java
@@ -6,12 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
