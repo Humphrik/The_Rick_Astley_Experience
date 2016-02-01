15555c47b3bcbdf0fc4ec1e4e83fec90a194a24e
alexanderwarren <arocks124@gmail.com>
2016-02-01 10:59:54 -0500

    Put the classes into different files and updated the TODO list.

--- Gui Test/src/window/Window.java
+++ Gui Test/src/window/Window.java
@@ -2,8 +2,6 @@ package window;
 
 import java.awt.Button;
 import java.awt.Label;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -162,45 +160,3 @@ public class Window {
 	}
 }
 
-class Listener implements ActionListener { // For when the button is pressed.
-	private double click;
-	private int clickCount = 0;
-	private static Icon hauntedIcon = new ImageIcon(Window.hauntedURL); // ooooOOOOhhh.
-
-	public void actionPerformed(ActionEvent e) {
-		click = Math.random();
-		clickCount++; // Adds to click count.
-		if (clickCount == 50) {
-			Window.label.setText("You suck."); // Easter egg.
-		} else if (clickCount == 90) {
-			Window.label.setText("Give up."); // Easter egg.
-		} else if (clickCount >= 120 && clickCount <= 128) {
-			Window.label.setText("N3VER GONN4 G1VE Y0U UP!1!!11!");
-		} else if (clickCount > 128 && clickCount <= 136) {
-			Window.label.setText("N#V_R G0NN4 L33t y0# d_WN!1!1!");
-		} else if (clickCount > 136 && clickCount <= 144) {
-			Window.label.setText("N___R GOn54 RN aR##Nd");
-		} else if (clickCount > 144 && clickCount <= 152) {
-			Window.label.setText("AND D3S3RT YYYYYYY UUI YP UI YIUY JKSDH");
-		} else if (clickCount > 152) { // You've gone too far...
-			Window.label2.setIcon(hauntedIcon);
-			Window.spooky = false;
-		} else {
-			Window.label.setText("" + clickCount); // Displays the number of
-													// clicks (usually.)
-		}
-		if (Window.debugging)
-			System.out.println(click);
-		if (click >= 0.995) { // When you are lucky enough....
-			Window.label.setText("You may Leave.");
-			Window.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Window
-																			// will
-																			// no
-																			// longer
-																			// "reopen."
-			Window.button.setEnabled(false); // Turns off button.
-
-		}
-
-	}
-}
